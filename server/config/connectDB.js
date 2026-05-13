2a8fb99969dc7494cbd45b87f51aae37f524cefa
Jesús Viafara <jealvito@gmail.com>
2026-05-13 06:47:03 -0500

    AWS preparation

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,12 +1,14 @@
-import { connect } from 'mongoose';
+import { connect } from 'mongoose'
+import dns from 'node:dns'
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
+dns.setServers(['1.1.1.1', '8.8.8.8'])
 const connectDB = () => {
-    connect(url)
-        .then(() => {
-            console.log('Connection to DB established');
-        })
-        .catch((e) => console.log(e.message));
-};
+  connect(url)
+    .then(() => {
+      console.log('Connection to DB established')
+    })
+    .catch(e => console.log(e.message))
+}
 
-connectDB();
+connectDB()
