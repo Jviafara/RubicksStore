322f5e9f3e2a38d3b268e85a089c2fdaf4b5a98b
Jesús Viafara <jealvito@gmail.com>
2026-05-13 07:03:43 -0500

    AWS

--- client/src/api/client/publicClient.js
+++ client/src/api/client/publicClient.js
@@ -1,31 +1,32 @@
-import axios from 'axios';
-import queryString from 'query-string';
+import axios from 'axios'
+import queryString from 'query-string'
 
-const baseURL = 'https://rubikstore.onrender.com/api/v1';
+const baseURL = 'http://localhost:8080/api/v1'
+// const baseURL = 'https://rubikstore.onrender.com/api/v1';
 // const baseURL = 'https://rubicksstore.up.railway.app/api/v1';
 
 const publicClient = axios.create({
-    baseURL,
-    paramsSerializer: { encode: (params) => queryString.stringify(params) },
-});
+  baseURL,
+  paramsSerializer: { encode: params => queryString.stringify(params) },
+})
 
-publicClient.interceptors.request.use(async (config) => {
-    return {
-        ...config,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-});
+publicClient.interceptors.request.use(async config => {
+  return {
+    ...config,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+})
 
 publicClient.interceptors.response.use(
-    (response) => {
-        if (response && response.data) return response.data;
-        return response;
-    },
-    (err) => {
-        throw err.response.data;
-    }
-);
+  response => {
+    if (response && response.data) return response.data
+    return response
+  },
+  err => {
+    throw err.response.data
+  },
+)
 
-export default publicClient;
+export default publicClient
