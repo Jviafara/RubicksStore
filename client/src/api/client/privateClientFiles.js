86c1dabbb86eb4bee5c735896a61f336aba3ae63
Jesús Viafara <jealvito@gmail.com>
2026-05-13 07:53:37 -0500

    AWS

--- client/src/api/client/privateClientFiles.js
+++ client/src/api/client/privateClientFiles.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseURL = 'http://localhost:8080/api/v1'
+const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:8080/api/v1'
 // const baseURL = 'https://rubikstore.onrender.com/api/v1';
 // const baseURL = 'https://rubicksstore.up.railway.app/api/v1';
 
